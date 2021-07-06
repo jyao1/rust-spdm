65a109cccaa4f1a37739c3ee8205bc9d44d2a7b5
Xiaohanjlll <hanx.xiao@intel.com>
2021-07-06 17:33:18 +0800

    add test

---
+++ spdmlib/src/testlib.rs
@@ -0,0 +1,224 @@
+use crate::common::SpdmDeviceIo;
+use crate::msgs::*;
+use crate::common::SpdmTransportEncap;
+use crate::error::SpdmResult;
+
+use crate::{common};
+use crate::{spdm_err, spdm_result_err};
+
+use codec::enum_builder;
+use codec::{Codec, Reader, Writer};
+
+pub fn create_info() -> (common::SpdmConfigInfo, common::SpdmProvisionInfo) {
+    let config_info = common::SpdmConfigInfo {
+        spdm_version: [SpdmVersion::SpdmVersion10, SpdmVersion::SpdmVersion11],
+        rsp_capabilities: SpdmResponseCapabilityFlags::CERT_CAP
+        | SpdmResponseCapabilityFlags::CHAL_CAP
+        | SpdmResponseCapabilityFlags::MEAS_CAP_SIG
+        | SpdmResponseCapabilityFlags::MEAS_FRESH_CAP
+        | SpdmResponseCapabilityFlags::ENCRYPT_CAP
+        | SpdmResponseCapabilityFlags::MAC_CAP
+        | SpdmResponseCapabilityFlags::KEY_EX_CAP
+        | SpdmResponseCapabilityFlags::PSK_CAP_WITH_CONTEXT
+        | SpdmResponseCapabilityFlags::ENCAP_CAP
+        | SpdmResponseCapabilityFlags::HBEAT_CAP
+        | SpdmResponseCapabilityFlags::KEY_UPD_CAP, 
+        rsp_ct_exponent: 0,
+        measurement_specification: SpdmMeasurementSpecification::DMTF,
+        measurement_hash_algo: SpdmMeasurementHashAlgo::TPM_ALG_SHA_384,
+        base_asym_algo: SpdmBaseAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384,
+        base_hash_algo: SpdmBaseHashAlgo::TPM_ALG_SHA_384,
+        dhe_algo: SpdmDheAlgo::SECP_384_R1,
+
+        aead_algo: SpdmAeadAlgo::AES_256_GCM,
+        req_asym_algo: SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048,
+        key_schedule_algo: SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE,
+        ..Default::default()
+    };
+
+    let mut my_cert_chain_data = SpdmCertChainData {
+        ..Default::default()
+    };
+
+    let ca_file_path = "../TestKey/EcP384/ca.cert.der";
+    let ca_cert = std::fs::read(ca_file_path).expect("unable to read ca cert!");
+    let inter_file_path = "../TestKey/EcP384/inter.cert.der";
+    let inter_cert = std::fs::read(inter_file_path).expect("unable to read inter cert!");
+    let leaf_file_path = "../TestKey/EcP384/end_responder.cert.der";
+    let leaf_cert = std::fs::read(leaf_file_path).expect("unable to read leaf cert!");
+
+    let ca_len = ca_cert.len();
+    let inter_len = inter_cert.len();
+    let leaf_len = leaf_cert.len();
+
+    my_cert_chain_data.data_size = (ca_len + inter_len + leaf_len) as u16;
+    my_cert_chain_data.data[0..ca_len].copy_from_slice(ca_cert.as_ref());
+    my_cert_chain_data.data[ca_len..(ca_len + inter_len)].copy_from_slice(inter_cert.as_ref());
+    my_cert_chain_data.data[(ca_len + inter_len)..(ca_len + inter_len + leaf_len)]
+        .copy_from_slice(leaf_cert.as_ref());
+
+    let provision_info = common::SpdmProvisionInfo {
+        my_cert_chain_data: Some(my_cert_chain_data),
+        my_cert_chain: None,
+        peer_cert_chain_data: None,
+        peer_cert_chain_root_hash: None,
+    };
+
+    (config_info, provision_info)
+}
+// #[derive(Debug, Copy, Clone, Default)]
+pub struct MySpdmDeviceIo;
+
+impl SpdmDeviceIo for MySpdmDeviceIo {
+    fn send(&mut self, _buffer: &[u8]) -> SpdmResult {
+        todo!()
+    }
+
+    fn receive(&mut self, _buffer: &mut [u8]) -> Result<usize, usize> {
+        todo!()
+    }
+
+    fn flush_all(&mut self) -> SpdmResult {
+        todo!()
+    }
+}
+
+enum_builder! {
+    @U16
+    EnumName: PciDoeVendorId;
+    EnumVal{
+        PciDoeVendorIdPciSig => 0x0001
+    }
+}
+
+enum_builder! {
+    @U8
+    EnumName: PciDoeDataObjectType;
+    EnumVal{
+        PciDoeDataObjectTypeDoeDiscovery => 0x00,
+        PciDoeDataObjectTypeSpdm => 0x01,
+        PciDoeDataObjectTypeSecuredSpdm => 0x02
+    }
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+pub struct PciDoeMessageHeader {
+    pub vendor_id: PciDoeVendorId,
+    pub data_object_type: PciDoeDataObjectType,
+    pub payload_length: u32, // in bytes
+}
+
+impl Codec for PciDoeMessageHeader {
+    fn encode(&self, bytes: &mut Writer) {
+        self.vendor_id.encode(bytes);
+        self.data_object_type.encode(bytes);
+        0u8.encode(bytes);
+        let mut length = (self.payload_length + 8) >> 2;
+        if length > 0x100000 {
+            panic!();
+        }
+        if length == 0x100000 {
+            length = 0;
+        }
+        length.encode(bytes);
+    }
+
+    fn read(r: &mut Reader) -> Option<PciDoeMessageHeader> {
+        let vendor_id = PciDoeVendorId::read(r)?;
+        let data_object_type = PciDoeDataObjectType::read(r)?;
+        u8::read(r)?;
+        let mut length = u32::read(r)?;
+        if length == 0 {
+            length = 0x40000;
+        }
+        if length < 2 {
+            return None;
+        }
+        let payload_length = (length << 2) - 8;
+        Some(PciDoeMessageHeader {
+            vendor_id,
+            data_object_type,
+            payload_length,
+        })
+    }
+}
+
+pub struct PciDoeTransportEncap {}
+
+impl SpdmTransportEncap for PciDoeTransportEncap {
+    fn encap(
+        &mut self,
+        spdm_buffer: &[u8],
+        transport_buffer: &mut [u8],
+        secured_message: bool,
+    ) -> SpdmResult<usize> {
+        let payload_len = spdm_buffer.len();
+        let aligned_payload_len = (payload_len + 3) / 4 * 4;
+        let mut writer = Writer::init(&mut transport_buffer[..]);
+        let pcidoe_header = PciDoeMessageHeader {
+            vendor_id: PciDoeVendorId::PciDoeVendorIdPciSig,
+            data_object_type: if secured_message {
+                PciDoeDataObjectType::PciDoeDataObjectTypeSecuredSpdm
+            } else {
+                PciDoeDataObjectType::PciDoeDataObjectTypeSpdm
+            },
+            payload_length: aligned_payload_len as u32,
+        };
+        pcidoe_header.encode(&mut writer);
+        let header_size = writer.used();
+        if transport_buffer.len() < header_size + aligned_payload_len {
+            return spdm_result_err!(EINVAL);
+        }
+        transport_buffer[header_size..(header_size + payload_len)].copy_from_slice(spdm_buffer);
+        Ok(header_size + aligned_payload_len)
+    }
+
+    fn decap(
+        &mut self,
+        transport_buffer: &[u8],
+        spdm_buffer: &mut [u8],
+    ) -> SpdmResult<(usize, bool)> {
+        let mut reader = Reader::init(&transport_buffer[..]);
+        let secured_message;
+        match PciDoeMessageHeader::read(&mut reader) {
+            Some(pcidoe_header) => {
+                match pcidoe_header.vendor_id {
+                    PciDoeVendorId::PciDoeVendorIdPciSig => {}
+                    _ => return spdm_result_err!(EINVAL),
+                }
+                match pcidoe_header.data_object_type {
+                    PciDoeDataObjectType::PciDoeDataObjectTypeSpdm => secured_message = false,
+                    PciDoeDataObjectType::PciDoeDataObjectTypeSecuredSpdm => secured_message = true,
+                    _ => return spdm_result_err!(EINVAL),
+                }
+            }
+            None => return spdm_result_err!(EIO),
+        }
+        let header_size = reader.used();
+        let payload_size = transport_buffer.len() - header_size;
+        if spdm_buffer.len() < payload_size {
+            return spdm_result_err!(EINVAL);
+        }
+        let payload = &transport_buffer[header_size..];
+        spdm_buffer[..payload_size].copy_from_slice(payload);
+        Ok((payload_size, secured_message))
+    }
+
+    fn encap_app(&mut self, spdm_buffer: &[u8], app_buffer: &mut [u8]) -> SpdmResult<usize> {
+        app_buffer[0..spdm_buffer.len()].copy_from_slice(spdm_buffer);
+        Ok(spdm_buffer.len())
+    }
+
+    fn decap_app(&mut self, app_buffer: &[u8], spdm_buffer: &mut [u8]) -> SpdmResult<usize> {
+        spdm_buffer[0..app_buffer.len()].copy_from_slice(app_buffer);
+        Ok(app_buffer.len())
+    }
+
+    fn get_sequence_number_count(&mut self) -> u8 {
+        0
+    }
+
+    fn get_max_random_count(&mut self) -> u16 {
+        0
+    }
+}
