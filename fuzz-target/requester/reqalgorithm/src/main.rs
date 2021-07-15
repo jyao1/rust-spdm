0281dd2afedad779069cdc9725de39dfb1a7e3b3
haowei <weix.hao@intel.com>
2021-07-16 01:37:17 +0800

    update fuzzing

--- fuzz-target/requester/reqalgorithm/src/main.rs
+++ fuzz-target/requester/reqalgorithm/src/main.rs
@@ -15,7 +15,6 @@ fn fuzz_send_receive_spdm_algorithm(fuzzdata: &[u8]) {
 
     spdmlib::crypto::asym_sign::register(ASYM_SIGN_IMPL);
 
-
     let mut responder = responder::ResponderContext::new(
         &mut device_io_responder,
         pcidoe_transport_encap,
@@ -23,16 +22,80 @@ fn fuzz_send_receive_spdm_algorithm(fuzzdata: &[u8]) {
         rsp_provision_info,
     );
 
+    let message_a:&[u8; 38] = &[
+        16, 132, 0, 0, 17, 4, 0, 0, 0, 2, 0, 16, 0, 17, 17, 225, 0, 0, 0, 0, 0, 0, 198, 118, 0, 0,
+        17, 97, 0, 0, 0, 0, 0, 0, 246, 122, 0, 0,
+    ];
+
+    responder.common.reset_runtime_info();
+    responder.common.runtime_info.message_b.append_message(message_a);
+
+    // capability_rsp
+    responder.common.negotiate_info.req_ct_exponent_sel = 0;
+    responder.common.negotiate_info.req_capabilities_sel = SpdmRequestCapabilityFlags::CERT_CAP
+    | SpdmRequestCapabilityFlags::CHAL_CAP
+    | SpdmRequestCapabilityFlags::ENCRYPT_CAP
+    | SpdmRequestCapabilityFlags::MAC_CAP
+    //| SpdmRequestCapabilityFlags::MUT_AUTH_CAP
+    | SpdmRequestCapabilityFlags::KEY_EX_CAP
+    | SpdmRequestCapabilityFlags::PSK_CAP
+    | SpdmRequestCapabilityFlags::ENCAP_CAP
+    | SpdmRequestCapabilityFlags::HBEAT_CAP
+    | SpdmRequestCapabilityFlags::KEY_UPD_CAP;
+    responder.common.negotiate_info.rsp_ct_exponent_sel = 0;
+    responder.common.negotiate_info.rsp_capabilities_sel = SpdmResponseCapabilityFlags::CERT_CAP
+    | SpdmResponseCapabilityFlags::CHAL_CAP
+    | SpdmResponseCapabilityFlags::MEAS_CAP_SIG
+    | SpdmResponseCapabilityFlags::MEAS_FRESH_CAP
+    | SpdmResponseCapabilityFlags::ENCRYPT_CAP
+    | SpdmResponseCapabilityFlags::MAC_CAP
+    //| SpdmResponseCapabilityFlags::MUT_AUTH_CAP
+    | SpdmResponseCapabilityFlags::KEY_EX_CAP
+    | SpdmResponseCapabilityFlags::PSK_CAP_WITH_CONTEXT
+    | SpdmResponseCapabilityFlags::ENCAP_CAP
+    | SpdmResponseCapabilityFlags::HBEAT_CAP
+    | SpdmResponseCapabilityFlags::KEY_UPD_CAP;
 
     let pcidoe_transport_encap2 = &mut PciDoeTransportEncap {};
-    let mut device_io_requester = fake_device_io::FakeSpdmDeviceIo::new(&shared_buffer, &mut responder);
+    let mut device_io_requester =
+        fake_device_io::FakeSpdmDeviceIo::new(&shared_buffer, &mut responder);
 
     let mut requester = requester::RequesterContext::new(
         &mut device_io_requester,
         pcidoe_transport_encap2,
         req_config_info,
-        req_provision_info   
+        req_provision_info,
     );
+    requester.common.reset_runtime_info();
+    requester.common.runtime_info.message_a.append_message(message_a);
+
+    // capability 
+    requester.common.negotiate_info.req_ct_exponent_sel = 0;
+    requester.common.negotiate_info.req_capabilities_sel =
+        requester.common.config_info.req_capabilities;
+    requester.common.negotiate_info.req_capabilities_sel = SpdmRequestCapabilityFlags::CERT_CAP
+    | SpdmRequestCapabilityFlags::CHAL_CAP
+    | SpdmRequestCapabilityFlags::ENCRYPT_CAP
+    | SpdmRequestCapabilityFlags::MAC_CAP
+    //| SpdmRequestCapabilityFlags::MUT_AUTH_CAP
+    | SpdmRequestCapabilityFlags::KEY_EX_CAP
+    | SpdmRequestCapabilityFlags::PSK_CAP
+    | SpdmRequestCapabilityFlags::ENCAP_CAP
+    | SpdmRequestCapabilityFlags::HBEAT_CAP
+    | SpdmRequestCapabilityFlags::KEY_UPD_CAP;
+    requester.common.negotiate_info.rsp_ct_exponent_sel = 0;
+    requester.common.negotiate_info.rsp_capabilities_sel = SpdmResponseCapabilityFlags::CERT_CAP
+    | SpdmResponseCapabilityFlags::CHAL_CAP
+    | SpdmResponseCapabilityFlags::MEAS_CAP_SIG
+    | SpdmResponseCapabilityFlags::MEAS_FRESH_CAP
+    | SpdmResponseCapabilityFlags::ENCRYPT_CAP
+    | SpdmResponseCapabilityFlags::MAC_CAP
+    //| SpdmResponseCapabilityFlags::MUT_AUTH_CAP
+    | SpdmResponseCapabilityFlags::KEY_EX_CAP
+    | SpdmResponseCapabilityFlags::PSK_CAP_WITH_CONTEXT
+    | SpdmResponseCapabilityFlags::ENCAP_CAP
+    | SpdmResponseCapabilityFlags::HBEAT_CAP
+    | SpdmResponseCapabilityFlags::KEY_UPD_CAP;
 
     let _ = requester.send_receive_spdm_algorithm().is_err();
 }
