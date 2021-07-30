0d1930b3fbca50100797ec05187d0ea0e3b748c0
haowei <weix.hao@intel.com>
2021-07-31 01:24:52 +0800

    update fuzzing

--- test_spdm_coverage.sh
+++ test_spdm_coverage.sh
@@ -10,4 +10,4 @@ cargo build -p spdm-responder-emu -p spdm-requester-emu
 cargo run -p spdm-responder-emu & 
 cargo run -p spdm-requester-emu
 
-grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/source_coverage/
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/test_spdm_coverage/
