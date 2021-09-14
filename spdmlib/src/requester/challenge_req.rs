801adef3b1166efc93e65a5d79eba9fd55f9587c
Xiaohanjlll <hanx.xiao@intel.com>
2021-09-15 02:58:40 +0800

    add uint test

--- spdmlib/src/requester/challenge_req.rs
+++ spdmlib/src/requester/challenge_req.rs
@@ -109,21 +109,38 @@ impl<'a> RequesterContext<'a> {
 #[cfg(test)]
 mod tests_requester {
     use super::*;
+    use crate::crypto::SpdmCryptoRandom;
     use crate::testlib::*;
     use crate::{crypto, responder};
 
     #[test]
+    #[ignore]
     fn test_case0_handle_spdm_algorithm() {
+        let data = &mut [
+            0x1, 0x0, 0x1, 0x0, 0x30, 0x0, 0x0, 0x0, 0x11, 0x3, 0x0, 0x1, 0x28, 0xaf, 0x70, 0x27,
+            0xbc, 0x2d, 0x95, 0xb5, 0xa0, 0xe4, 0x26, 0x4, 0xc5, 0x8c, 0x5c, 0x3c, 0xbf, 0xa2,
+            0xc8, 0x24, 0xa6, 0x30, 0xca, 0x2f, 0xf, 0x4a, 0x79, 0x35, 0x57, 0xfb, 0x39, 0x3b,
+            0xdd, 0x8a, 0xc8, 0x8a, 0x92, 0xd8, 0xa3, 0x70, 0x17, 0x12, 0x83, 0x9b, 0x66, 0xe1,
+            0x3a, 0x3a, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x0, 0x0, 0x3, 0x76, 0xd, 0x57, 0x9b, 0xaf, 0xe9,
+            0x6f, 0xc2, 0x5c, 0x2f, 0x3a, 0xfb, 0x81, 0xb, 0x4f, 0xa4, 0x5a, 0x65, 0x4a, 0xc8,
+            0x64, 0x38, 0x91, 0xb1, 0x89, 0x8d, 0x42, 0xe9, 0xff, 0x55, 0xb, 0xfd, 0xb1, 0xe1,
+            0x3c, 0x19, 0x1f, 0x1e, 0x8, 0xa2, 0x78, 0xd, 0xf3, 0x6, 0x6a, 0xfa, 0xe, 0xee, 0xde,
+            0x27, 0x9, 0xb3, 0x20, 0xa1, 0xf5, 0x8d, 0x6e, 0xfc, 0x8a, 0x30, 0x91, 0x5, 0x80, 0xae,
+            0x89, 0xb4, 0xee, 0x38, 0xcc, 0x92, 0x8e, 0x5e, 0x5b, 0x25, 0x10, 0xdb, 0xd8, 0x32,
+            0x11, 0xd7, 0xf8, 0x23, 0x76, 0x49, 0x3d, 0x96, 0x7e, 0xb3, 0x22, 0x4c, 0x5d, 0x50,
+            0x79, 0x71, 0x98, 0x0, 0x0,
+        ];
+
         let (rsp_config_info, rsp_provision_info) = create_info();
         let (req_config_info, req_provision_info) = create_info();
-
         let shared_buffer = SharedBuffer::new();
-        let data = &mut [100u8; 100];
         let mut device_io_responder = SpdmDeviceIoReceve::new(&shared_buffer, data);
         let pcidoe_transport_encap = &mut PciDoeTransportEncap {};
 
         crypto::asym_sign::register(ASYM_SIGN_IMPL);
-        crypto::rand::register(DEFAULT_TEST);
+        crypto::rand::register(RAND_TEST);
 
         let mut responder = responder::ResponderContext::new(
             &mut device_io_responder,
@@ -147,10 +164,7 @@ mod tests_requester {
         responder.common.negotiate_info.aead_sel = SpdmAeadAlgo::AES_256_GCM;
         responder.common.negotiate_info.req_asym_sel = SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048;
         responder.common.negotiate_info.key_schedule_sel = SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE;
-        responder.common.provision_info.my_cert_chain = Some(SpdmCertChainData {
-            data_size: 100u16,
-            data: [100u8; config::MAX_SPDM_CERT_CHAIN_DATA_SIZE],
-        });
+        responder.common.provision_info.my_cert_chain = Some(REQ_CERT_CHAIN_DATA);
 
         let pcidoe_transport_encap2 = &mut PciDoeTransportEncap {};
         let mut device_io_requester = FakeSpdmDeviceIo::new(&shared_buffer, &mut responder);
@@ -177,16 +191,26 @@ mod tests_requester {
         requester.common.negotiate_info.aead_sel = SpdmAeadAlgo::AES_256_GCM;
         requester.common.negotiate_info.req_asym_sel = SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048;
         requester.common.negotiate_info.key_schedule_sel = SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE;
-        requester.common.provision_info.my_cert_chain = Some(SpdmCertChainData {
-            data_size: 100u16,
-            data: [100u8; config::MAX_SPDM_CERT_CHAIN_DATA_SIZE],
-        });
+
+        requester.common.peer_info.peer_cert_chain.cert_chain = REQ_CERT_CHAIN_DATA;
+
         let status = requester
             .send_receive_spdm_challenge(
                 0,
                 SpdmMeasurementSummaryHashType::SpdmMeasurementSummaryHashTypeNone,
             )
-            .is_err();
+            .is_ok();
         assert!(status);
+    }
+
+    pub static RAND_TEST: SpdmCryptoRandom = SpdmCryptoRandom {
+        get_random_cb: get_random,
+    };
+
+    fn get_random(data: &mut [u8]) -> SpdmResult<usize> {
+        let rand_data = [0xff; SPDM_NONCE_SIZE];
+        data.copy_from_slice(&rand_data);
+
+        Ok(data.len())
     }
 }
