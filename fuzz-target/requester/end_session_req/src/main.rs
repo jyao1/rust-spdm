62907d59147ffbe02b0ac2dd73ccd8997feb53b7
haowqs <Weix.Hao@intel.com>
2021-09-15 05:34:55 +0800

    Modify possible data errors

--- fuzz-target/requester/end_session_req/src/main.rs
+++ fuzz-target/requester/end_session_req/src/main.rs
@@ -2,164 +2,170 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 
-use fuzzlib::{*, spdmlib::session::{SpdmSession, SpdmSessionState}};
+use fuzzlib::{
+    spdmlib::session::{SpdmSession, SpdmSessionState},
+    *,
+};
 
 fn fuzz_send_receive_spdm_end_session(fuzzdata: &[u8]) {
     let (rsp_config_info, rsp_provision_info) = rsp_create_info();
     let (req_config_info, req_provision_info) = req_create_info();
+    let (rsp_config_info1, rsp_provision_info1) = rsp_create_info();
+    let (req_config_info1, req_provision_info1) = req_create_info();
 
-    let shared_buffer = SharedBuffer::new();
-    let mut device_io_responder = FuzzSpdmDeviceIoReceve::new(&shared_buffer, fuzzdata);
+    {
+        let shared_buffer = SharedBuffer::new();
+        let mut device_io_responder = FakeSpdmDeviceIoReceve::new(&shared_buffer);
 
-    let pcidoe_transport_encap = &mut PciDoeTransportEncap {};
+        let pcidoe_transport_encap = &mut PciDoeTransportEncap {};
 
-    spdmlib::crypto::asym_sign::register(ASYM_SIGN_IMPL);
+        spdmlib::crypto::asym_sign::register(ASYM_SIGN_IMPL);
 
-    let mut responder = responder::ResponderContext::new(
-        &mut device_io_responder,
-        pcidoe_transport_encap,
-        rsp_config_info,
-        rsp_provision_info,
-    );
+        let mut responder = responder::ResponderContext::new(
+            &mut device_io_responder,
+            pcidoe_transport_encap,
+            rsp_config_info,
+            rsp_provision_info,
+        );
 
-    // capability_rsp
-    responder.common.negotiate_info.req_ct_exponent_sel = 0;
-    responder.common.negotiate_info.req_capabilities_sel = SpdmRequestCapabilityFlags::CERT_CAP
-    | SpdmRequestCapabilityFlags::CHAL_CAP
-    | SpdmRequestCapabilityFlags::ENCRYPT_CAP
-    | SpdmRequestCapabilityFlags::MAC_CAP
-    //| SpdmRequestCapabilityFlags::MUT_AUTH_CAP
-    | SpdmRequestCapabilityFlags::KEY_EX_CAP
-    | SpdmRequestCapabilityFlags::PSK_CAP
-    | SpdmRequestCapabilityFlags::ENCAP_CAP
-    | SpdmRequestCapabilityFlags::HBEAT_CAP
-    | SpdmRequestCapabilityFlags::KEY_UPD_CAP;
-    responder.common.negotiate_info.rsp_ct_exponent_sel = 0;
-    responder.common.negotiate_info.rsp_capabilities_sel = SpdmResponseCapabilityFlags::CERT_CAP
-    | SpdmResponseCapabilityFlags::CHAL_CAP
-    | SpdmResponseCapabilityFlags::MEAS_CAP_SIG
-    | SpdmResponseCapabilityFlags::MEAS_FRESH_CAP
-    | SpdmResponseCapabilityFlags::ENCRYPT_CAP
-    | SpdmResponseCapabilityFlags::MAC_CAP
-    //| SpdmResponseCapabilityFlags::MUT_AUTH_CAP
-    | SpdmResponseCapabilityFlags::KEY_EX_CAP
-    | SpdmResponseCapabilityFlags::PSK_CAP_WITH_CONTEXT
-    | SpdmResponseCapabilityFlags::ENCAP_CAP
-    | SpdmResponseCapabilityFlags::HBEAT_CAP
-    | SpdmResponseCapabilityFlags::KEY_UPD_CAP;
+        // capability_rsp
+        responder.common.negotiate_info.req_ct_exponent_sel = 0;
 
-    // algorithm_rsp
-    responder
-        .common
-        .negotiate_info
-        .measurement_specification_sel = SpdmMeasurementSpecification::DMTF;
-    responder.common.negotiate_info.measurement_hash_sel = SpdmMeasurementHashAlgo::TPM_ALG_SHA_384;
-    responder.common.negotiate_info.base_hash_sel = SpdmBaseHashAlgo::TPM_ALG_SHA_384;
-    responder.common.negotiate_info.base_asym_sel = SpdmBaseAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384;
-    responder.common.negotiate_info.dhe_sel = SpdmDheAlgo::SECP_384_R1;
-    responder.common.negotiate_info.aead_sel = SpdmAeadAlgo::AES_256_GCM;
-    responder.common.negotiate_info.req_asym_sel = SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048;
-    responder.common.negotiate_info.key_schedule_sel = SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE;
+        responder.common.negotiate_info.base_hash_sel = SpdmBaseHashAlgo::TPM_ALG_SHA_384;
+        responder.common.negotiate_info.base_asym_sel =
+            SpdmBaseAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384;
+        responder.common.negotiate_info.dhe_sel = SpdmDheAlgo::SECP_384_R1;
+        responder.common.negotiate_info.aead_sel = SpdmAeadAlgo::AES_256_GCM;
+        responder.common.negotiate_info.req_asym_sel = SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048;
+        responder.common.negotiate_info.key_schedule_sel = SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE;
 
-    responder.common.provision_info.my_cert_chain = Some(REQ_CERT_CHAIN_DATA);
 
-    responder.common.reset_runtime_info();
-    responder
-        .common
-        .runtime_info
-        .message_a
-        .append_message(MESSAGE_A);
-    responder
-        .common
-        .runtime_info
-        .message_b
-        .append_message(MESSAGE_B);
-    responder
-        .common
-        .runtime_info
-        .message_c
-        .append_message(MESSAGE_C);
+        responder.common.reset_runtime_info();
 
-    let pcidoe_transport_encap2 = &mut PciDoeTransportEncap {};
-    let mut device_io_requester =
-        fake_device_io::FakeSpdmDeviceIo::new(&shared_buffer, &mut responder);
+        responder.common.session[0].setup(4294901758).unwrap();
+        responder.common.session[0].set_session_state(SpdmSessionState::SpdmSessionHandshaking);
+        responder.common.session[0].set_crypto_param(
+            SpdmBaseHashAlgo::TPM_ALG_SHA_384,
+            SpdmDheAlgo::SECP_384_R1,
+            SpdmAeadAlgo::AES_256_GCM,
+            SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE,
+        );
 
-    let mut requester = requester::RequesterContext::new(
-        &mut device_io_requester,
-        pcidoe_transport_encap2,
-        req_config_info,
-        req_provision_info,
-    );
+        let pcidoe_transport_encap2 = &mut PciDoeTransportEncap {};
+        let mut device_io_requester =
+            fake_device_io::FakeSpdmDeviceIo::new(&shared_buffer, &mut responder);
 
-    requester.common.negotiate_info.req_ct_exponent_sel = 0;
-    requester.common.negotiate_info.req_capabilities_sel = SpdmRequestCapabilityFlags::CERT_CAP
-    | SpdmRequestCapabilityFlags::CHAL_CAP
-    | SpdmRequestCapabilityFlags::ENCRYPT_CAP
-    | SpdmRequestCapabilityFlags::MAC_CAP
-    //| SpdmRequestCapabilityFlags::MUT_AUTH_CAP
-    | SpdmRequestCapabilityFlags::KEY_EX_CAP
-    | SpdmRequestCapabilityFlags::PSK_CAP
-    | SpdmRequestCapabilityFlags::ENCAP_CAP
-    | SpdmRequestCapabilityFlags::HBEAT_CAP
-    | SpdmRequestCapabilityFlags::KEY_UPD_CAP;
-    requester.common.negotiate_info.rsp_ct_exponent_sel = 0;
-    requester.common.negotiate_info.rsp_capabilities_sel = SpdmResponseCapabilityFlags::CERT_CAP
-    | SpdmResponseCapabilityFlags::CHAL_CAP
-    | SpdmResponseCapabilityFlags::MEAS_CAP_SIG
-    | SpdmResponseCapabilityFlags::MEAS_FRESH_CAP
-    | SpdmResponseCapabilityFlags::ENCRYPT_CAP
-    | SpdmResponseCapabilityFlags::MAC_CAP
-    //| SpdmResponseCapabilityFlags::MUT_AUTH_CAP
-    | SpdmResponseCapabilityFlags::KEY_EX_CAP
-    | SpdmResponseCapabilityFlags::PSK_CAP_WITH_CONTEXT
-    | SpdmResponseCapabilityFlags::ENCAP_CAP
-    | SpdmResponseCapabilityFlags::HBEAT_CAP
-    | SpdmResponseCapabilityFlags::KEY_UPD_CAP;
+        let mut requester = requester::RequesterContext::new(
+            &mut device_io_requester,
+            pcidoe_transport_encap2,
+            req_config_info,
+            req_provision_info,
+        );
 
-    //algorithm_req
-    requester
-        .common
-        .negotiate_info
-        .measurement_specification_sel = SpdmMeasurementSpecification::DMTF;
-    requester.common.negotiate_info.measurement_hash_sel = SpdmMeasurementHashAlgo::TPM_ALG_SHA_384;
-    requester.common.negotiate_info.base_hash_sel = SpdmBaseHashAlgo::TPM_ALG_SHA_384;
-    requester.common.negotiate_info.base_asym_sel = SpdmBaseAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384;
-    requester.common.negotiate_info.dhe_sel = SpdmDheAlgo::SECP_384_R1;
-    requester.common.negotiate_info.aead_sel = SpdmAeadAlgo::AES_256_GCM;
-    requester.common.negotiate_info.req_asym_sel = SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048;
-    requester.common.negotiate_info.key_schedule_sel = SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE;
+        requester.common.negotiate_info.measurement_hash_sel =
+            SpdmMeasurementHashAlgo::TPM_ALG_SHA_384;
+        requester.common.negotiate_info.base_hash_sel = SpdmBaseHashAlgo::TPM_ALG_SHA_384;
+        requester.common.negotiate_info.base_asym_sel =
+            SpdmBaseAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384;
+        requester.common.negotiate_info.dhe_sel = SpdmDheAlgo::SECP_384_R1;
+        requester.common.negotiate_info.aead_sel = SpdmAeadAlgo::AES_256_GCM;
+        requester.common.negotiate_info.req_asym_sel = SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048;
+        requester.common.negotiate_info.key_schedule_sel = SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE;
 
-    requester.common.peer_info.peer_cert_chain.cert_chain = REQ_CERT_CHAIN_DATA;
 
-    requester.common.reset_runtime_info();
-    requester
-        .common
-        .runtime_info
-        .message_a
-        .append_message(MESSAGE_A);
-    requester
-        .common
-        .runtime_info
-        .message_b
-        .append_message(MESSAGE_B);
-    requester
-        .common
-        .runtime_info
-        .message_c
-        .append_message(MESSAGE_C);
-    
-    requester.common.session = [SpdmSession::new(); 4];
-    requester.common.session[0].setup(4294901758).unwrap();
-    requester.common.session[0].set_crypto_param(
-        SpdmBaseHashAlgo::TPM_ALG_SHA_384,
-        SpdmDheAlgo::SECP_384_R1,
-        SpdmAeadAlgo::AES_256_GCM,
-        SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE,
-    );
-    requester.common.session[0].set_session_state(SpdmSessionState::SpdmSessionEstablished);
+        requester.common.reset_runtime_info();
 
-    let _ = requester
-        .send_receive_spdm_end_session(4294901758);
+
+        requester.common.session = [SpdmSession::new(); 4];
+        requester.common.session[0].setup(4294901758).unwrap();
+        requester.common.session[0].set_crypto_param(
+            SpdmBaseHashAlgo::TPM_ALG_SHA_384,
+            SpdmDheAlgo::SECP_384_R1,
+            SpdmAeadAlgo::AES_256_GCM,
+            SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE,
+        );
+        requester.common.session[0].set_session_state(SpdmSessionState::SpdmSessionHandshaking);
+
+        let _ = requester.send_receive_spdm_end_session(4294901758);
+    }
+
+    {
+        let shared_buffer = SharedBuffer::new();
+        let mut device_io_responder = FuzzSpdmDeviceIoReceve::new(&shared_buffer, fuzzdata);
+
+        let pcidoe_transport_encap = &mut PciDoeTransportEncap {};
+
+        spdmlib::crypto::asym_sign::register(ASYM_SIGN_IMPL);
+
+        let mut responder = responder::ResponderContext::new(
+            &mut device_io_responder,
+            pcidoe_transport_encap,
+            rsp_config_info1,
+            rsp_provision_info1,
+        );
+
+        // algorithm_rsp
+        responder.common.negotiate_info.measurement_hash_sel =
+            SpdmMeasurementHashAlgo::TPM_ALG_SHA_384;
+        responder.common.negotiate_info.base_hash_sel = SpdmBaseHashAlgo::TPM_ALG_SHA_384;
+        responder.common.negotiate_info.base_asym_sel =
+            SpdmBaseAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384;
+        responder.common.negotiate_info.dhe_sel = SpdmDheAlgo::SECP_384_R1;
+        responder.common.negotiate_info.aead_sel = SpdmAeadAlgo::AES_256_GCM;
+        responder.common.negotiate_info.req_asym_sel = SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048;
+        responder.common.negotiate_info.key_schedule_sel = SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE;
+
+        responder.common.reset_runtime_info();
+
+        responder.common.session[0].setup(4294901758).unwrap();
+        responder.common.session[0].set_session_state(SpdmSessionState::SpdmSessionHandshaking);
+        responder.common.session[0].set_crypto_param(
+            SpdmBaseHashAlgo::TPM_ALG_SHA_384,
+            SpdmDheAlgo::SECP_384_R1,
+            SpdmAeadAlgo::AES_256_GCM,
+            SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE,
+        );
+
+        let pcidoe_transport_encap2 = &mut PciDoeTransportEncap {};
+        let mut device_io_requester =
+            fake_device_io::FakeSpdmDeviceIo::new(&shared_buffer, &mut responder);
+
+        let mut requester = requester::RequesterContext::new(
+            &mut device_io_requester,
+            pcidoe_transport_encap2,
+            req_config_info1,
+            req_provision_info1,
+        );
+
+
+
+        //algorithm_req
+        requester.common.negotiate_info.measurement_hash_sel =
+            SpdmMeasurementHashAlgo::TPM_ALG_SHA_384;
+        requester.common.negotiate_info.base_hash_sel = SpdmBaseHashAlgo::TPM_ALG_SHA_384;
+        requester.common.negotiate_info.base_asym_sel =
+            SpdmBaseAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384;
+        requester.common.negotiate_info.dhe_sel = SpdmDheAlgo::SECP_384_R1;
+        requester.common.negotiate_info.aead_sel = SpdmAeadAlgo::AES_256_GCM;
+        requester.common.negotiate_info.req_asym_sel = SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048;
+        requester.common.negotiate_info.key_schedule_sel = SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE;
+
+        requester.common.peer_info.peer_cert_chain.cert_chain = REQ_CERT_CHAIN_DATA;
+
+        requester.common.reset_runtime_info();
+
+        requester.common.session = [SpdmSession::new(); 4];
+        requester.common.session[0].setup(4294901758).unwrap();
+        requester.common.session[0].set_crypto_param(
+            SpdmBaseHashAlgo::TPM_ALG_SHA_384,
+            SpdmDheAlgo::SECP_384_R1,
+            SpdmAeadAlgo::AES_256_GCM,
+            SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE,
+        );
+        requester.common.session[0].set_session_state(SpdmSessionState::SpdmSessionHandshaking);
+
+        let _ = requester.send_receive_spdm_end_session(4294901758);
+    }
 }
 
 fn main() {
@@ -183,7 +189,11 @@ fn main() {
         let args: Vec<String> = std::env::args().collect();
         if args.len() < 2 {
             // Here you can replace the single-step debugging value in the fuzzdata array.
-            let fuzzdata = [17,46,43];
+            let fuzzdata = [
+                0x1, 0x0, 0x2, 0x0, 0x9, 0x0, 0x0, 0x0, 0xfe, 0xff, 0xfe, 0xff, 0x16, 0x0, 0xca,
+                0xa7, 0x51, 0x51, 0x4d, 0x60, 0xb5, 0x2e, 0x73, 0xb7, 0xfc, 0xd, 0xba, 0x50, 0x24,
+                0xf1, 0xa8, 0xee, 0x87, 0x5d, 0x37, 0xca,
+            ];
             fuzz_send_receive_spdm_end_session(&fuzzdata);
         } else {
             let path = &args[1];
--- fuzz_run.sh
+++ fuzz_run.sh
@@ -67,7 +67,7 @@ do
     then
     screen -dmS ${cmds[$i]}
     fi
-    screen -x -S ${cmds[$i]} -p 0 -X stuff "cargo afl fuzz -i fuzz-target/in -o fuzz-target/out/${cmds[$i]} target/debug/${cmds[$i]}"
+    screen -x -S ${cmds[$i]} -p 0 -X stuff "cargo afl fuzz -i fuzz-target/in/${cmds[$i]} -o fuzz-target/out/${cmds[$i]} target/debug/${cmds[$i]}"
     screen -x -S ${cmds[$i]} -p 0 -X stuff $'\n'
     sleep 1800
     screen -S ${cmds[$i]} -X quit
