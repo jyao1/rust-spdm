a56033db0cf8afe3f26da0b14536a2b6c75e5a40
Jiewen Yao <jiewen.yao@intel.com>
2023-05-15 20:02:35 +0800

    Add version check for slot_id in get_measurement
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- spdmlib/src/message/measurement_test.rs
+++ spdmlib/src/message/measurement_test.rs
@@ -15,6 +15,7 @@ use testlib::{create_spdm_context, DeviceIO, TransportEncap};
 fn test_measurement_struct() {
     create_spdm_context!(context);
     let context = &mut context;
+    context.negotiate_info.spdm_version_sel = SpdmVersion::SpdmVersion11;
 
     // Validate SpdmMeasurementAttributes::SIGNATURE_REQUESTED length
     let u8_slice = &mut [0u8; 4 + 32 + 1];
