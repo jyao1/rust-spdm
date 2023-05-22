6512286cdf1beb7e741afdce84f50377aee4b432
Jiewen Yao <jiewen.yao@intel.com>
2023-05-22 16:42:09 +0800

    add error message for dispatch_message
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- spdmlib/src/responder/error_rsp.rs
+++ spdmlib/src/responder/error_rsp.rs
@@ -38,6 +38,48 @@ impl<'a> ResponderContext<'a> {
     }
 }
 
+impl<'a> ResponderContext<'a> {
+    pub fn handle_error_request(
+        &mut self,
+        error_code: SpdmErrorCode,
+        session_id: Option<u32>,
+        bytes: &[u8],
+    ) {
+        let mut send_buffer = [0u8; config::MAX_SPDM_MSG_SIZE];
+        let mut writer = Writer::init(&mut send_buffer);
+        self.write_error_response(error_code, bytes, &mut writer);
+        if let Some(session_id) = session_id {
+            let _ = self.send_secured_message(session_id, writer.used_slice(), false);
+        } else {
+            let _ = self.send_message(writer.used_slice());
+        }
+    }
+
+    pub fn write_error_response(
+        &mut self,
+        error_code: SpdmErrorCode,
+        bytes: &[u8],
+        writer: &mut Writer,
+    ) {
+        let mut reader = Reader::init(bytes);
+        let message_header = SpdmMessageHeader::read(&mut reader);
+        if let Some(message_header) = message_header {
+            if message_header.version != self.common.negotiate_info.spdm_version_sel {
+                self.write_spdm_error(SpdmErrorCode::SpdmErrorVersionMismatch, 0, writer);
+                return;
+            }
+            let error_data = if error_code == SpdmErrorCode::SpdmErrorUnsupportedRequest {
+                message_header.request_response_code.get_u8()
+            } else {
+                0u8
+            };
+            self.write_spdm_error(error_code, error_data, writer);
+        } else {
+            self.write_spdm_error(SpdmErrorCode::SpdmErrorInvalidRequest, 0, writer);
+        }
+    }
+}
+
 #[cfg(all(test,))]
 mod tests_responder {
     use super::*;
