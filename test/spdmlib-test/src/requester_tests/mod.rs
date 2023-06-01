13e81f9675eb2cc2e471328c06752fc1c6ae5d29
xiaotia3 <xiaotian.chen@intel.com>
2023-06-01 11:14:13 +0800

    Test: move integration tests to test dir
    Signed-off-by: xiaotia3 <xiaotian.chen@intel.com>

---
+++ test/spdmlib-test/src/requester_tests/mod.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+#![forbid(unsafe_code)]
+
+#[cfg(test)]
+#[cfg(feature = "hashed-transcript-data")]
+mod challenge_req;
