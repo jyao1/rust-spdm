0e3931bdcc350234c97b78eca02a460139b508eb
xiaotia3 <xiaotian.chen@intel.com>
2023-06-16 11:51:34 +0800

    Fuzz: sync test util changes
    Signed-off-by: xiaotia3 <xiaotian.chen@intel.com>

--- test/spdmlib-test/src/common/util.rs
+++ test/spdmlib-test/src/common/util.rs
@@ -6,6 +6,7 @@
 
 use super::USE_ECDSA;
 use crate::common::device_io::MySpdmDeviceIo;
+use crate::common::secret_callback::SECRET_ASYM_IMPL_INSTANCE;
 use crate::common::transport::PciDoeTransportEncap;
 use codec::{Reader, Writer};
 use spdmlib::common::{
@@ -131,23 +132,29 @@ pub fn new_spdm_message(value: SpdmMessage, mut context: SpdmContext) -> SpdmMes
 }
 
 pub fn req_create_info() -> (SpdmConfigInfo, SpdmProvisionInfo) {
+    let req_capabilities = SpdmRequestCapabilityFlags::CERT_CAP
+        | SpdmRequestCapabilityFlags::CHAL_CAP
+        | SpdmRequestCapabilityFlags::ENCRYPT_CAP
+        | SpdmRequestCapabilityFlags::MAC_CAP
+        | SpdmRequestCapabilityFlags::KEY_EX_CAP
+        | SpdmRequestCapabilityFlags::PSK_CAP
+        | SpdmRequestCapabilityFlags::ENCAP_CAP
+        | SpdmRequestCapabilityFlags::HBEAT_CAP
+        // | SpdmResponseCapabilityFlags::HANDSHAKE_IN_THE_CLEAR_CAP
+        // | SpdmResponseCapabilityFlags::PUB_KEY_ID_CAP
+        | SpdmRequestCapabilityFlags::KEY_UPD_CAP;
+    let req_capabilities = if cfg!(feature = "mut-auth") {
+        req_capabilities | SpdmRequestCapabilityFlags::MUT_AUTH_CAP
+    } else {
+        req_capabilities
+    };
     let config_info = SpdmConfigInfo {
         spdm_version: [
             SpdmVersion::SpdmVersion10,
             SpdmVersion::SpdmVersion11,
             SpdmVersion::SpdmVersion12,
         ],
-        req_capabilities: SpdmRequestCapabilityFlags::CERT_CAP
-        | SpdmRequestCapabilityFlags::CHAL_CAP
-        | SpdmRequestCapabilityFlags::ENCRYPT_CAP
-        | SpdmRequestCapabilityFlags::MAC_CAP
-        //| SpdmRequestCapabilityFlags::MUT_AUTH_CAP
-        | SpdmRequestCapabilityFlags::KEY_EX_CAP
-        | SpdmRequestCapabilityFlags::PSK_CAP
-        | SpdmRequestCapabilityFlags::ENCAP_CAP
-        | SpdmRequestCapabilityFlags::HBEAT_CAP
-        | SpdmRequestCapabilityFlags::KEY_UPD_CAP, // | SpdmRequestCapabilityFlags::HANDSHAKE_IN_THE_CLEAR_CAP
-        // | SpdmRequestCapabilityFlags::PUB_KEY_ID_CAP
+        req_capabilities: req_capabilities,
         req_ct_exponent: 0,
         measurement_specification: SpdmMeasurementSpecification::DMTF,
         base_asym_algo: if USE_ECDSA {
@@ -158,7 +165,11 @@ pub fn req_create_info() -> (SpdmConfigInfo, SpdmProvisionInfo) {
         base_hash_algo: SpdmBaseHashAlgo::TPM_ALG_SHA_384,
         dhe_algo: SpdmDheAlgo::SECP_384_R1,
         aead_algo: SpdmAeadAlgo::AES_256_GCM,
-        req_asym_algo: SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048,
+        req_asym_algo: if USE_ECDSA {
+            SpdmReqAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384
+        } else {
+            SpdmReqAsymAlgo::TPM_ALG_RSASSA_3072
+        },
         key_schedule_algo: SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE,
         opaque_support: SpdmOpaqueSupport::OPAQUE_DATA_FMT1,
         data_transfer_size: config::MAX_SPDM_MSG_SIZE as u32,
@@ -203,35 +214,69 @@ pub fn req_create_info() -> (SpdmConfigInfo, SpdmProvisionInfo) {
     peer_root_cert_data.data_size = (ca_len) as u16;
     peer_root_cert_data.data[0..ca_len].copy_from_slice(ca_cert.as_ref());
 
-    let provision_info = SpdmProvisionInfo {
-        my_cert_chain_data: [None, None, None, None, None, None, None, None],
-        my_cert_chain: [None, None, None, None, None, None, None, None],
-        peer_root_cert_data: Some(peer_root_cert_data),
+    let provision_info = if cfg!(feature = "mut-auth") {
+        spdmlib::secret::asym_sign::register(SECRET_ASYM_IMPL_INSTANCE.clone());
+        let mut my_cert_chain_data = SpdmCertChainData {
+            ..Default::default()
+        };
+
+        my_cert_chain_data.data_size = (ca_len + inter_len + leaf_len) as u16;
+        my_cert_chain_data.data[0..ca_len].copy_from_slice(ca_cert.as_ref());
+        my_cert_chain_data.data[ca_len..(ca_len + inter_len)].copy_from_slice(inter_cert.as_ref());
+        my_cert_chain_data.data[(ca_len + inter_len)..(ca_len + inter_len + leaf_len)]
+            .copy_from_slice(leaf_cert.as_ref());
+
+        SpdmProvisionInfo {
+            my_cert_chain_data: [
+                Some(my_cert_chain_data),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            ],
+            my_cert_chain: [None, None, None, None, None, None, None, None],
+            peer_root_cert_data: Some(peer_root_cert_data),
+        }
+    } else {
+        SpdmProvisionInfo {
+            my_cert_chain_data: [None, None, None, None, None, None, None, None],
+            my_cert_chain: [None, None, None, None, None, None, None, None],
+            peer_root_cert_data: Some(peer_root_cert_data),
+        }
     };
 
     (config_info, provision_info)
 }
 
 pub fn rsp_create_info() -> (SpdmConfigInfo, SpdmProvisionInfo) {
+    let rsp_capabilities = SpdmResponseCapabilityFlags::CERT_CAP
+        | SpdmResponseCapabilityFlags::CHAL_CAP
+        | SpdmResponseCapabilityFlags::MEAS_CAP_SIG
+        | SpdmResponseCapabilityFlags::MEAS_FRESH_CAP
+        | SpdmResponseCapabilityFlags::ENCRYPT_CAP
+        | SpdmResponseCapabilityFlags::MAC_CAP
+        | SpdmResponseCapabilityFlags::KEY_EX_CAP
+        | SpdmResponseCapabilityFlags::PSK_CAP_WITH_CONTEXT
+        | SpdmResponseCapabilityFlags::ENCAP_CAP
+        | SpdmResponseCapabilityFlags::HBEAT_CAP
+        // | SpdmResponseCapabilityFlags::HANDSHAKE_IN_THE_CLEAR_CAP
+        // | SpdmResponseCapabilityFlags::PUB_KEY_ID_CAP
+        | SpdmResponseCapabilityFlags::KEY_UPD_CAP;
+    let rsp_capabilities = if cfg!(feature = "mut-auth") {
+        rsp_capabilities | SpdmResponseCapabilityFlags::MUT_AUTH_CAP
+    } else {
+        rsp_capabilities
+    };
     let config_info = SpdmConfigInfo {
         spdm_version: [
             SpdmVersion::SpdmVersion10,
             SpdmVersion::SpdmVersion11,
             SpdmVersion::SpdmVersion12,
         ],
-        rsp_capabilities: SpdmResponseCapabilityFlags::CERT_CAP
-        | SpdmResponseCapabilityFlags::CHAL_CAP
-        | SpdmResponseCapabilityFlags::MEAS_CAP_SIG
-        | SpdmResponseCapabilityFlags::MEAS_FRESH_CAP
-        | SpdmResponseCapabilityFlags::ENCRYPT_CAP
-        | SpdmResponseCapabilityFlags::MAC_CAP
-        //| SpdmResponseCapabilityFlags::MUT_AUTH_CAP
-        | SpdmResponseCapabilityFlags::KEY_EX_CAP
-        | SpdmResponseCapabilityFlags::PSK_CAP_WITH_CONTEXT
-        | SpdmResponseCapabilityFlags::ENCAP_CAP
-        | SpdmResponseCapabilityFlags::HBEAT_CAP
-        | SpdmResponseCapabilityFlags::KEY_UPD_CAP, // | SpdmResponseCapabilityFlags::HANDSHAKE_IN_THE_CLEAR_CAP
-        // | SpdmResponseCapabilityFlags::PUB_KEY_ID_CAP
+        rsp_capabilities: rsp_capabilities,
         rsp_ct_exponent: 0,
         measurement_specification: SpdmMeasurementSpecification::DMTF,
         measurement_hash_algo: SpdmMeasurementHashAlgo::TPM_ALG_SHA_384,
@@ -243,7 +288,11 @@ pub fn rsp_create_info() -> (SpdmConfigInfo, SpdmProvisionInfo) {
         base_hash_algo: SpdmBaseHashAlgo::TPM_ALG_SHA_384,
         dhe_algo: SpdmDheAlgo::SECP_384_R1,
         aead_algo: SpdmAeadAlgo::AES_256_GCM,
-        req_asym_algo: SpdmReqAsymAlgo::TPM_ALG_RSAPSS_2048,
+        req_asym_algo: if USE_ECDSA {
+            SpdmReqAsymAlgo::TPM_ALG_ECDSA_ECC_NIST_P384
+        } else {
+            SpdmReqAsymAlgo::TPM_ALG_RSASSA_3072
+        },
         key_schedule_algo: SpdmKeyScheduleAlgo::SPDM_KEY_SCHEDULE,
         opaque_support: SpdmOpaqueSupport::OPAQUE_DATA_FMT1,
         data_transfer_size: config::MAX_SPDM_MSG_SIZE as u32,
